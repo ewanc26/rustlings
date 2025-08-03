a6308fbf48b56f1276217d3a83cba9b45686251c
Ewan <git@ewancroft.uk>
2025-08-03 21:29:58 +0100

    complete section 13

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -21,7 +21,9 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
 
     // TODO: Handle the error case as described above.
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?; // So here's my fix
+    // If the parse fails, it will return an error immediately
+    // and not try to multiply and add.
 
     Ok(qty * cost_per_item + processing_fee)
 }
